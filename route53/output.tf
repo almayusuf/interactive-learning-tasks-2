91552f045ee4316e2ffdfb105a7ff524e3a6d643
kayahuseyin64 <kayahuseyin64.com>
2021-11-03 14:01:36 +0000

    hm

--- route53/output.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_route53_record" "www" {
-  zone_id = Z09485621JN7TS90NI9SM
-  name    = "blog.www.kayahuseyin.net"
-}
--- route53/route53.tf
+++ route53/route53.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "www" {
   zone_id = Z09485621JN7TS90NI9SM
   name    = "blog.www.kayahuseyin.net"
-  records = [127.0.0.1]
+  records = ["127.0.0.1"]
 }
