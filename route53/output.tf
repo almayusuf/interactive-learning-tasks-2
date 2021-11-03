f3717fab7d7b0f87cde21891880b3522d061de1f
kayahuseyin64 <kayahuseyin64.com>
2021-11-03 17:53:32 +0000

    ss

--- route53/output.tf
+++ route53/output.tf
@@ -1,4 +1,12 @@
 output "zone_id" {
-  value       = "Z09485621JN7TS90NI9SM"
-  description = "The hosted zone id"
-}
+ description = " The zone ID number "
+ value = aws_route53_record.dev-ns.zone_id
+ }
+ output "name" {
+ description = " Name "
+ value = aws_route53_zone.dev.name
+ }
+ output "records" {
+ description = " Display records "
+ value = aws_route53_record.dev-ns.records
+ }
--- route53/route53.tf
+++ route53/route53.tf
@@ -1,5 +1,13 @@
-resource "aws_route53_record" "www" {
-  zone_id = "Z09485621JN7TS90NI9SM"
-  name    = "blog.www.kayahuseyin.net"
+resource "aws_route53_zone" "main" {
+  name = "kayahuseyin.net"
+}
+resource "aws_route53_zone" "dev" {
+  name = "blog.kayahuseu=yin.net"
+}
+resource "aws_route53_record" "dev-ns" {
+  zone_id = "Z05859503VOVDG70P4C98"
+  name    = "blog.kayahuseyin.net"
+  type    = "NS"
+  ttl     = "30"
   records = ["127.0.0.1"]
 }
