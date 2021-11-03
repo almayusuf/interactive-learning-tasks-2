a274adc1d9ed9692b57c30ba35da5781ee075e79
kayahuseyin64 <kayahuseyin64.com>
2021-11-03 04:13:48 +0000

    gfd

---
+++ route53/output.tf
@@ -0,0 +1,4 @@
+resource "aws_route53_record" "www" {
+  zone_id = Z09485621JN7TS90NI9SM
+  name    = "blog.www.kayahuseyin.net"
+}
--- route53/route53.tf
+++ route53/route53.tf
@@ -2,8 +2,4 @@ resource "aws_route53_record" "www" {
   zone_id = Z09485621JN7TS90NI9SM
   name    = "blog.www.kayahuseyin.net"
   records = [127.0.0.1]
-  records = 127.0.0.1.dev.ns-1925.awsdns-48.co.uk
-ns-1128.awsdns-13.org
-ns-114.awsdns-14.com
-ns-728.awsdns-27.net
 }
