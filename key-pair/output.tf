270c07121447d34230cfa50818c6f11c975bf926
kayahuseyin64 <kayahuseyin64.com>
2021-11-04 15:55:30 +0000

    push

---
+++ key-pair/output.tf
@@ -0,0 +1,16 @@
+output "region" {
+    description = "region"
+    value       = "aws.region"
+}
+
+
+output "key_name" {
+    description = "key-name"
+    value       = "aws_key_pair.ilearning-wordpress.key_name"
+}
+
+output "key_id" {
+    description = "key-id"
+    value       = "aws_key_pair.ilearning-wordpress.public_key"
+}
+
