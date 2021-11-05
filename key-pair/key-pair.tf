1a4597101ae9706e960183965d9c6d9d51572482
kayahuseyin64 <kayahuseyin64.com>
2021-11-05 02:02:48 +0000

    gh

--- key-pair/key-pair.tf
+++ key-pair/key-pair.tf
@@ -2,7 +2,7 @@
 resource "aws_key_pair" "ilearning-wordpress" {
   key_name   = "ilearning-wordpress"
   public_key = file("~/.ssh/id_rsa.pub")
-  key_pair_id = "key-04b5615a29216e7c6"
+  
 }
 
 data "aws_region"   "current"  {}
--- key-pair/output.tf
+++ key-pair/output.tf
@@ -1,16 +1,17 @@
 output "region" {
     description = "region"
-    value       =  "data.aws_region.current"
+    value       =  "us-east-2"
 }
 
 
 output "key_name" {
     description = "key-name"
-    value       = "aws_key_pair.ilearning-wordpress.key_name"
+    value       = aws_key_pair.ilearning-wordpress.key_name
 }
 
 output "key_id" {
     description = "keypair-id"
-    value       = "aws_key_pair.ilearning-wordpress.key_pair_id"
+    value       = aws_key_pair.ilearning-wordpress.key_pair_id
 }
+
 
